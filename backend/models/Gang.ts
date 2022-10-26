12bcb04214c72d2ba56fb9d7cbf9b47f687882af
Aarón Carrizal <aaronk4572@gmail.com>
2022-10-25 22:43:19 -0500

    fixed gang schema

--- backend/models/Gang.ts
+++ backend/models/Gang.ts
@@ -1,10 +1,47 @@
-import {Schema , model} from 'mongoose'
+import {Schema , model, Types} from 'mongoose'
 
 const gangSchema = new Schema({
     name:{
         type: String,
         required: true,
         trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    leader: {
+        type: Types.ObjectId,
+        ref: "User"
+    },
+    members: {
+        type: [Types.ObjectId],
+        ref: "User"
+    },
+    numMembers: {
+        type: Number
+    },
+    ageAverage: {
+        type: Number
+    },
+    reunionPlace: {
+        type : Types.ObjectId,
+        ref: "Place"
+    },
+    dangerousness: {
+        type: Number,
+    },
+    rivalries: {
+        type: [Types.ObjectId],
+        ref: "Gang"
+    },
+    alliances: {
+        type: [Types.ObjectId],
+        ref: "Gang"
+    },
+    crimes: {
+        type: [Types.ObjectId],
+        ref: "Crime"
     }
 })
 
