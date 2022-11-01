1821c2077421c46a2f9d9360e5631145baa1c8a4
Aarón Carrizal <aaron.carrizal@fyware.com>
2022-11-01 15:25:37 -0600

    added dependencies of new interfaces

--- frontend/src/interfaces/Gang.ts
+++ frontend/src/interfaces/Gang.ts
@@ -1,11 +1,14 @@
+import { Place } from './Place'
+import { Member } from './Member'
+
 export interface Gang {
     name: string,
     description: string,
-    // leader: Member,
+    leader: Member,
     members: [],
     numMembers: number,
     ageAverage: number,
-    // reunionPlace: Place,
+    reunionPlace: Place,
     dengerousness: number,
     rivalries: [],
     alliances: [],
