2fd01ceb9c00aeac81863dd0a2170d2ba2dd0c76
Aarón Carrizal <aaron.carrizal@fyware.com>
2022-11-01 16:02:40 -0600

    reworked routes to individual files

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-
-import routes from './routes'
+import baseRoutes from './routes/index'
+import gangRoutes from './routes/gangRoutes'
+import memberRoutes from './routes/memberRoutes'
 
 const app = express()
 app.use(cors())
 app.use(morgan('dev'))
 app.use(express.json())
-app.use("/api", routes)
+app.use("/api", baseRoutes)
+app.use("/api", gangRoutes)
+app.use("/api", memberRoutes)
 export default app
