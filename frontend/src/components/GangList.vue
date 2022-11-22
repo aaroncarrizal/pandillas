3951040d395c98eca35781acc1a079446e88d458
Aaron Carrizal <180816@upslp.edu.mx>
2022-11-21 22:34:07 -0600

    fixed gang listing

--- frontend/src/components/GangList.vue
+++ frontend/src/components/GangList.vue
@@ -1,13 +1,39 @@
 <template>
     <div class="container">
         <div class="row">
+            <h1>Pandillas registradas</h1>
+        </div>
+        <div class="row">
             <template v-for="(gang, index) in gangs" :key="index">
                 <div class="col-4-sm-12 my-4">
                     <div class="card">
                         <div class="card-body">
-                            <h5 class="card-title">{{ gang.name }}</h5>
-                            <p class="card-text">{{ gang }}</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h3 class="card-title">{{ gang.name }}</h3>
+                            <h4>{{ gang.description }}</h4>
+                            <div class="row">
+                                <div class="col-6">
+                                    <h5>Alianzas</h5>
+                                    <ul>
+                                        <li v-for="(alliance,i) in gang.alliances" :key="i">
+                                            {{ alliance }}
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-6">
+                                    <h5>Rivalidades</h5>
+                                    <ul>
+                                        <li v-for="(rivalrie,i) in gang.rivalries" :key="i">
+                                            {{ rivalrie }}
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="row my-3">
+                                <div class="progress" style="height: 20px;">
+                                    <div class="progress-bar" role="progressbar" :style="`width: ${gang.dengerousness}%;`"></div>
+                                </div>
+                            </div>
+                            <a :href="'/gangs/' + gang._id" class="btn btn-primary">Ver detalles</a>
                         </div>
                     </div>
                 </div>
