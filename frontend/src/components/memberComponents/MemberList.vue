ca66ec1696eb6aacc2c0ab1db25607e19d5ed84c
Aaron Carrizal <180816@upslp.edu.mx>
2022-11-27 17:44:55 -0600

    replaced gang data

--- frontend/src/components/memberComponents/MemberList.vue
+++ frontend/src/components/memberComponents/MemberList.vue
@@ -4,36 +4,13 @@
             <h1>Miembros registrados</h1>
         </div>
         <div class="row">
-            <template v-for="(gang, index) in gangs" :key="index">
+            <template v-for="(member, index) in members" :key="index">
                 <div class="col-4-sm-12 my-4">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="card-title">{{ gang.name }}</h3>
-                            <h4>{{ gang.description }}</h4>
-                            <div class="row">
-                                <div class="col-6">
-                                    <h5>Alianzas</h5>
-                                    <ul>
-                                        <li v-for="(alliance,i) in gang.alliances" :key="i">
-                                            {{ alliance }}
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div class="col-6">
-                                    <h5>Rivalidades</h5>
-                                    <ul>
-                                        <li v-for="(rivalrie,i) in gang.rivalries" :key="i">
-                                            {{ rivalrie }}
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="row my-3">
-                                <div class="progress" style="height: 20px;">
-                                    <div class="progress-bar" role="progressbar" :style="`width: ${gang.dangerousness}%;`"></div>
-                                </div>
-                            </div>
-                            <a :href="'/gangs/' + gang._id" class="btn btn-primary">Ver detalles</a>
+                            <h3 class="card-title">{{ member.name.firstName + ' ' + member.name.lastName}} </h3>
+                            <h4>{{ member.alias }}</h4>
+                            <a :href="'/members/' + member._id" class="btn btn-primary">Ver detalles</a>
                         </div>
                     </div>
                 </div>
@@ -44,23 +21,23 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { getGangs } from '@/services/GangService'
-import { Gang } from '@/interfaces/Gang'
+import { getMembers } from '@/services/MemberService'
+import { Member } from '@/interfaces/Member'
 
 export default defineComponent({
     data(){
         return {
-            gangs: [] as Gang[]
+            members: [] as Member[]
         }
     },
     mounted(){
-        this.loadGangs()
+        this.loadMembers()
     },
     methods:{
-        async loadGangs(){
+        async loadMembers(){
             try {
-                const res = await getGangs()
-                this.gangs = res.data
+                const res = await getMembers()
+                this.members = res.data
             } catch (err) {
                 console.log(err)
             }
