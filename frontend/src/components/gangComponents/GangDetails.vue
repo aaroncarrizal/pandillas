8dae1d6bb37a0e7ba31d966694244e1ea21c12e8
Aaron Carrizal <180816@upslp.edu.mx>
2022-11-29 00:41:08 -0600

    added GangDetails

--- frontend/src/components/gangComponents/GangDetails.vue
+++ frontend/src/components/gangComponents/GangDetails.vue
@@ -1,5 +1,128 @@
 <template>
     <div class="container">
-        details
+        <div class="row">
+            <div class="col-4-sm-12 my-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h1 class="card-title">{{ gang.name }}</h1>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <h3>{{ gang.description }}</h3>
+                                    <h4>{{ gang.leader }}</h4>
+                                </div>
+                                <div class="col-sm-2">
+                                    <div class="d-grid gap-2">
+                                        <a :href="`/gangs/edit/${gangId}`" class="btn btn-primary" role="button">Editar</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-2">
+                                    <h5>Numero de miembros: {{ gang.numMembers }}</h5>
+                                </div>
+                                <div class="col-sm-2">
+                                    <h5>Promedio de edad: {{ gang.ageAverage }}</h5>
+                                </div>
+                                <div class="col-sm-2">
+                                    <h5>Peligrosidad: {{ gang.dangerousness }}</h5>
+                                </div>
+                                <div class="col-sm-6">
+                                    Lugar 
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <h4>Miembros</h4>
+                                    <ul class="list-group list-group-flush">
+                                        <template v-for="(member,i) in gang.members" :key="i">
+                                            <li class="list-group-item">
+                                                {{ member.name.firstName + ' ' + member.name.middleName + ' ' + member.name.lastName }}
+                                            </li>
+                                        </template>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-4">
+                                    <h4>Rivalidades</h4>
+                                    <ul class="list-group list-group-flush">
+                                        <template v-for="(rivalrie,i) in gang.rivalries" :key="i">
+                                            <li class="list-group-item">
+                                                {{ rivalrie }}
+                                            </li>
+                                        </template>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-4">
+                                    <h4>Alianzas</h4>
+                                    <ul class="list-group list-group-flush">
+                                        <template v-for="(alliance,i) in gang.alliances" :key="i">
+                                            <li class="list-group-item">
+                                                {{ alliance }}
+                                            </li>
+                                        </template>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <h4>Crimenes</h4>
+                                    <ul class="list-group list-group-flush">
+                                        <template v-for="(crime,i) in gang.crimes" :key="i">
+                                            <li class="list-group-item">
+                                                {{ crime }}
+                                            </li>
+                                        </template>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
     </div>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Gang } from '@/interfaces/Gang'
+import { getGang } from '@/services/GangService'
+import { getMembers} from '@/services/MemberService'
+import { Member } from '@/interfaces/Member'
+import { Place } from '@/interfaces/Place'
+import { Crime } from '@/interfaces/Crime'
+
+export default defineComponent({
+    data() {
+        return {
+            gang: {} as Gang,
+            members: [] as Member[],
+            gangId: ''
+        }
+    },
+    mounted(){
+        this.gangId = this.$route.params.id.toString()
+        this.loadMembers()
+        this.loadGang(this.gangId)
+    },
+    methods: {
+        async loadMembers(){
+            try {
+                const res = await getMembers()
+                this.members = res.data
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async loadGang(id: string){
+            try {
+                const res = await getGang(id)
+                this.gang = res.data
+            } catch (err) {
+                console.log(err)
+            }
+        },
+    }
+})
+</script>
