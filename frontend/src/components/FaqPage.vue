ce5459494dc74e20ed3232f7ddf238cb2fb52c1e
Eirikr06 <eri.456alp@gmail.com>
2022-11-29 00:46:55 -0600

    update faqpage added two questions

--- frontend/src/components/FaqPage.vue
+++ frontend/src/components/FaqPage.vue
@@ -1,11 +1,22 @@
 <template>
     <div class="container">
-        <p class="h1">FAQ</p>
+        <p class="h1 text-center">FAQ</p>
         <ol>
-            <li>¿Por qué estamos haciendo esto?</li>
-            <p>fack</p>
-            <li>Chingue a su madre el llamas</li>
-            <p>si</p>
+            <li>¿Qué se hace con la información que se recolecta?</li>
+            <p>La información que se recolecta en este sitio se emplea para entender las necesidades de nuestros usuarios y de esta forma proveer un mejor servicio; y de manera particular se emplea para:
+                <ul>
+                    <li>Registro de actividades en los servidores.</li>
+                    <li>Atención de problemas específicos del sitio y en su caso la corrección.</li>
+                    <li>Identificación y seguimiento de comentarios que se reciban a través de las formas de contacto y correo electrónico del sitio web.</li>
+                    <li>Utilizar la información para mejorar nuestros servicios en línea.</li>
+                </ul>
+            </p>
+            <li>¿Cómo utilizamos las cookies?</li>
+            <p>Una cookie es una pequeña pieza de información que es enviadas por el sitio web a su navegador. Las cookies se almacenan en el disco duro de su equipo y se utilizan para determinar sus preferencias cuando se conecta a los servicios de nuestros sitios, así como para rastrear determinados comportamientos ó actividades llevadas a cabo por usted dentro de nuestros sitios.</p>
+            <p>
+Las cookies permiten que los servicios que se ofrecen por el web, puedan adaptar su funcionamiento para que respondan a Usted como un usuario diferente del resto que visitan el sitio web y que esta identificación se refleje en que Usted tenga una experiencia personalizada al visitar este sitio.
+</p><p>
+Utilizamos cookies de registro del tráfico para identificar qué páginas se están visitando. Esto nos ayuda a analizar datos sobre el tráfico de la página web y mejorar la calidad de nuestro sitio web para adaptarlo y ajustar la información a las necesidades del cliente. Sólo utilizamos esta información para fines de análisis estadístico, por lo que después de esta acción los datos son eliminados automáticamente del sistema.</p>
         </ol>
     </div>
 </template>
