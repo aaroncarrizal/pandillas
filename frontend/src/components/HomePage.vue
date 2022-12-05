732156956310c1da668ab0b55f1d8c4cec145c9e
Aarón Carrizal <aaron.carrizal@fyware.com>
2022-12-05 14:06:04 -0600

    added basic gangs table

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,23 +1,69 @@
 <template>
-    <div>
-    <b-table striped hover :items="items"></b-table>
-    </div>
+    <b-container>
+        <h1>Pandillas registradas</h1>
+        <b-table :items="gangs" :fields="fields" :filter="filter" stacked="md" show-empty striped>
+            <template #cell(leader)="row">
+                <template v-if="row.item.leader">
+                    {{getFullName(row.item.leader)}}
+                </template>
+                <template v-else>
+                    Sin líder registrado
+                </template>
+            </template>
+            <template #cell(numMembers)="row">
+                <template v-if="row.item.numMembers">
+                    {{row.item.numMembers}}
+                </template>
+                <template v-else>
+                    0
+                </template>
+            </template>
+            <template #cell(reunionPlace)="row">
+                <template v-if="row.item.reunionPlace">
+                    {{row.item.reunionPlace.locality}}
+                </template>
+                <template v-else>
+                    Sin zona registrada
+                </template>
+            </template>
+        </b-table>
+    </b-container>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { getGangs } from '@/services/GangService'
+import { Gang } from '@/interfaces/Gang'
+import { Member } from '@/interfaces/Member'
+
 export default defineComponent({
     data(){
         return {
-            items: [
-                { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-                { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-                { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-                { age: 38, first_name: 'Jami', last_name: 'Carney' }
-            ]
+            gangs: [] as Gang[],
+            fields: [
+                { key: 'name', label: 'Nombre', sortable: true, },
+                { key: 'dangerousness', label: 'Peligrosidad', sortable: true},
+                { key: 'leader', label: 'Líder', sortable: true},
+                { key: 'numMembers', label: 'Número de miembros', sortable: true},
+                { key: 'reunionPlace', label: 'Zona de reunión', sortable: true},
+            ],
+            filter: undefined,
         }
     },
+    beforeMount(){
+        this.loadGangs()
+    },
     methods:{
-        
+        async loadGangs(){
+            try {
+                const res = await getGangs()
+                this.gangs = res.data
+            } catch (err) {
+                console.log(err)
+            }
+        },
+        getFullName(member: Member){
+            return member.name.firstName + ' ' + member.name.middleName + ' ' + member.name.lastName
+        },
     },
 })
 </script>
