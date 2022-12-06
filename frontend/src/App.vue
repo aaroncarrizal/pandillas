1b90f685e71f5ea8309f7c9f6b26af4fcfa80eb3
Aaron Carrizal <180816@upslp.edu.mx>
2022-12-05 23:38:06 -0600

    removed ;

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,9 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Navbar from "@/components/Navbar.vue";
+import { defineComponent } from 'vue'
+import Navbar from "@/components/Navbar.vue"
+
 
 export default defineComponent({
     name: 'App',
